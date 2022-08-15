093c234482d9585beb3286ef2f0997e4feb011a7
macheuz <machdosmatheus.0@gmail.com>
2022-08-15 09:13:34 -0300

    Populando formulario gestão de turmas

--- F_gestaoTurmas.Designer.cs
+++ F_gestaoTurmas.Designer.cs
@@ -30,20 +30,22 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgv_turmas = new System.Windows.Forms.DataGridView();
-            this.cb_professor = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.n_maxAlunos = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.cb_status = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.cb_horario = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btn_fechar = new System.Windows.Forms.Button();
             this.btn_imprimirTurma = new System.Windows.Forms.Button();
             this.btn_excluirTurma = new System.Windows.Forms.Button();
             this.btn_salvarEdicoes = new System.Windows.Forms.Button();
             this.btn_novaTurma = new System.Windows.Forms.Button();
+            this.cb_professor = new System.Windows.Forms.ComboBox();
+            this.cb_horario = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_dscTurma = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_turmas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.n_maxAlunos)).BeginInit();
             this.panel1.SuspendLayout();
@@ -70,18 +72,12 @@
             this.dgv_turmas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgv_turmas.Size = new System.Drawing.Size(487, 403);
             this.dgv_turmas.TabIndex = 6;
-            // 
-            // cb_professor
-            // 
-            this.cb_professor.Location = new System.Drawing.Point(505, 37);
-            this.cb_professor.Name = "cb_professor";
-            this.cb_professor.Size = new System.Drawing.Size(232, 20);
-            this.cb_professor.TabIndex = 11;
+            this.dgv_turmas.SelectionChanged += new System.EventHandler(this.dgv_turmas_SelectionChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(505, 21);
+            this.label2.Location = new System.Drawing.Point(502, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 12;
@@ -89,15 +85,15 @@
             // 
             // n_maxAlunos
             // 
-            this.n_maxAlunos.Location = new System.Drawing.Point(505, 85);
+            this.n_maxAlunos.Location = new System.Drawing.Point(502, 128);
             this.n_maxAlunos.Name = "n_maxAlunos";
             this.n_maxAlunos.Size = new System.Drawing.Size(105, 20);
-            this.n_maxAlunos.TabIndex = 13;
+            this.n_maxAlunos.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(505, 69);
+            this.label1.Location = new System.Drawing.Point(502, 112);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 14;
@@ -106,15 +102,15 @@
             // cb_status
             // 
             this.cb_status.FormattingEnabled = true;
-            this.cb_status.Location = new System.Drawing.Point(616, 85);
+            this.cb_status.Location = new System.Drawing.Point(613, 128);
             this.cb_status.Name = "cb_status";
             this.cb_status.Size = new System.Drawing.Size(121, 21);
-            this.cb_status.TabIndex = 15;
+            this.cb_status.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(613, 69);
+            this.label3.Location = new System.Drawing.Point(610, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 16;
@@ -123,19 +119,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(505, 116);
+            this.label4.Location = new System.Drawing.Point(502, 159);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 13);
             this.label4.TabIndex = 17;
             this.label4.Text = "Horario";
-            // 
-            // cb_horario
-            // 
-            this.cb_horario.FormattingEnabled = true;
-            this.cb_horario.Location = new System.Drawing.Point(505, 132);
-            this.cb_horario.Name = "cb_horario";
-            this.cb_horario.Size = new System.Drawing.Size(232, 21);
-            this.cb_horario.TabIndex = 18;
             // 
             // panel1
             // 
@@ -155,7 +143,7 @@
             this.btn_fechar.Location = new System.Drawing.Point(600, 3);
             this.btn_fechar.Name = "btn_fechar";
             this.btn_fechar.Size = new System.Drawing.Size(146, 23);
-            this.btn_fechar.TabIndex = 4;
+            this.btn_fechar.TabIndex = 10;
             this.btn_fechar.Text = "Fechar";
             this.btn_fechar.UseVisualStyleBackColor = true;
             // 
@@ -164,7 +152,7 @@
             this.btn_imprimirTurma.Location = new System.Drawing.Point(450, 3);
             this.btn_imprimirTurma.Name = "btn_imprimirTurma";
             this.btn_imprimirTurma.Size = new System.Drawing.Size(144, 23);
-            this.btn_imprimirTurma.TabIndex = 3;
+            this.btn_imprimirTurma.TabIndex = 9;
             this.btn_imprimirTurma.Text = "Imprimir Turma";
             this.btn_imprimirTurma.UseVisualStyleBackColor = true;
             // 
@@ -173,7 +161,7 @@
             this.btn_excluirTurma.Location = new System.Drawing.Point(300, 3);
             this.btn_excluirTurma.Name = "btn_excluirTurma";
             this.btn_excluirTurma.Size = new System.Drawing.Size(144, 23);
-            this.btn_excluirTurma.TabIndex = 2;
+            this.btn_excluirTurma.TabIndex = 8;
             this.btn_excluirTurma.Text = "Excluir Turma";
             this.btn_excluirTurma.UseVisualStyleBackColor = true;
             // 
@@ -182,7 +170,7 @@
             this.btn_salvarEdicoes.Location = new System.Drawing.Point(150, 3);
             this.btn_salvarEdicoes.Name = "btn_salvarEdicoes";
             this.btn_salvarEdicoes.Size = new System.Drawing.Size(144, 23);
-            this.btn_salvarEdicoes.TabIndex = 1;
+            this.btn_salvarEdicoes.TabIndex = 7;
             this.btn_salvarEdicoes.Text = "Salvar Edições";
             this.btn_salvarEdicoes.UseVisualStyleBackColor = true;
             // 
@@ -191,15 +179,50 @@
             this.btn_novaTurma.Location = new System.Drawing.Point(0, 4);
             this.btn_novaTurma.Name = "btn_novaTurma";
             this.btn_novaTurma.Size = new System.Drawing.Size(144, 23);
-            this.btn_novaTurma.TabIndex = 0;
+            this.btn_novaTurma.TabIndex = 6;
             this.btn_novaTurma.Text = "Nova Turma";
             this.btn_novaTurma.UseVisualStyleBackColor = true;
+            // 
+            // cb_professor
+            // 
+            this.cb_professor.FormattingEnabled = true;
+            this.cb_professor.Location = new System.Drawing.Point(505, 81);
+            this.cb_professor.Name = "cb_professor";
+            this.cb_professor.Size = new System.Drawing.Size(229, 21);
+            this.cb_professor.TabIndex = 2;
+            // 
+            // cb_horario
+            // 
+            this.cb_horario.FormattingEnabled = true;
+            this.cb_horario.Location = new System.Drawing.Point(502, 175);
+            this.cb_horario.Name = "cb_horario";
+            this.cb_horario.Size = new System.Drawing.Size(232, 21);
+            this.cb_horario.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(502, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Nome Turma";
+            // 
+            // tb_dscTurma
+            // 
+            this.tb_dscTurma.Location = new System.Drawing.Point(505, 41);
+            this.tb_dscTurma.Name = "tb_dscTurma";
+            this.tb_dscTurma.Size = new System.Drawing.Size(229, 20);
+            this.tb_dscTurma.TabIndex = 1;
             // 
             // F_gestaoTurmas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(749, 450);
+            this.Controls.Add(this.tb_dscTurma);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cb_professor);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.cb_horario);
             this.Controls.Add(this.label4);
@@ -208,7 +231,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.n_maxAlunos);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.cb_professor);
             this.Controls.Add(this.dgv_turmas);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -227,19 +249,21 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgv_turmas;
-        private System.Windows.Forms.TextBox cb_professor;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown n_maxAlunos;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cb_status;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox cb_horario;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btn_fechar;
         private System.Windows.Forms.Button btn_imprimirTurma;
         private System.Windows.Forms.Button btn_excluirTurma;
         private System.Windows.Forms.Button btn_salvarEdicoes;
         private System.Windows.Forms.Button btn_novaTurma;
+        private System.Windows.Forms.ComboBox cb_professor;
+        private System.Windows.Forms.ComboBox cb_horario;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_dscTurma;
     }
 }
--- F_gestaoTurmas.cs
+++ F_gestaoTurmas.cs
@@ -12,6 +12,7 @@ namespace banco_de_dados
 {
     public partial class F_gestaoTurmas : Form
     {
+        string idSelecionado;
         public F_gestaoTurmas()
         {
             InitializeComponent();
@@ -35,6 +36,65 @@ namespace banco_de_dados
             dgv_turmas.Columns[0].Width = 40;
             dgv_turmas.Columns[1].Width = 130;
             dgv_turmas.Columns[2].Width = 85;
+
+            //Popular comboBox professores
+            string vQueryProfessores = @"
+                SELECT  N_IDPROFESSOR,
+                        T_NOMEPROFESSOR
+                FROM tb_professores
+                ORDER BY N_IDPROFESSOR";
+            cb_professor.Items.Clear();
+            cb_professor.DataSource = Banco.dql(vQueryProfessores);
+            cb_professor.DisplayMember = "T_NOMEPROFESSOR";
+            cb_professor.ValueMember = "N_IDPROFESSOR";
+
+            //Popular combobox status
+
+            Dictionary<string, string> st = new Dictionary<string, string>();
+            st.Add("A","Ativa");
+            st.Add("P", "Paralisada");
+            st.Add("C", "Cancelada");
+            cb_status.Items.Clear();
+            cb_status.DataSource = new BindingSource(st, null);
+            cb_status.DisplayMember = "Value";
+            cb_status.ValueMember = "Key";
+
+            //Popular combobox de horario
+            string vQueryHorarios = @"
+                SELECT  *
+                FROM tb_horarios
+                ORDER BY T_DSCHORARIO";
+            cb_horario.Items.Clear();
+            cb_horario.DataSource = Banco.dql(vQueryHorarios);
+            cb_horario.DisplayMember = "T_DSCHORARIOS";
+            cb_horario.ValueMember = "N_IDHORARIO";
+
+        }
+
+        private void dgv_turmas_SelectionChanged(object sender, EventArgs e)
+        {
+            DataGridView dgv = (DataGridView)sender;
+            int contLinhas = dgv.SelectedRows.Count;
+            if(contLinhas > 0)
+            {
+                idSelecionado = dgv_turmas.Rows[dgv_turmas.SelectedRows[0].Index].Cells[0].Value.ToString();
+                string vqueryCampos = @"
+                 SELECT T_DSCTURMA,
+                        N_IDPROFESSOR,
+                        N_IDHORARIO,
+                        N_MAXALUNOS,
+                        T_STATUS
+                FROM tb_turmas
+                WHERE N_IDTURMA = " + idSelecionado;
+
+                DataTable dt = Banco.dql(vqueryCampos);
+                tb_dscTurma.Text = dt.Rows[0].Field<string>("T_DSCTURMA");
+                cb_professor.SelectedValue = dt.Rows[0].Field<Int64>("N_IDPROFESSOR").ToString();
+                n_maxAlunos.Value = dt.Rows[0].Field<Int64>("N_MAXALUNOS");
+                cb_status.SelectedValue = dt.Rows[0].Field<string>("T_STATUS");
+                cb_horario.SelectedValue = dt.Rows[0].Field<Int64>("N_IDHORARIO").ToString();
+            }
+
         }
     }
 }
