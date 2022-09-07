72f07ce3fabc9ec101539cd035a236f23bcbb458
macheuz <machdosmatheus.0@gmail.com>
2022-09-07 13:47:12 -0300

    Adicionando instalador ao projeto

--- Globais.cs
+++ Globais.cs
@@ -12,7 +12,8 @@ namespace banco_de_dados
         public static Boolean logado = false;
         public static int nivel = 0;
         //variavel para pegar o caminho de onde esta o executavel da aplicacao
-        public static string caminho = System.Environment.CurrentDirectory;
+        //public static string caminho = System.Environment.CurrentDirectory;
+        public static string caminho = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
         public static string nomeBanco = "banco_academia.db";
         //pegando o caminho do banco
         public static string caminhoBanco = caminho + @"\banco\";
